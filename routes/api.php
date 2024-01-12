3626b1dede2b668f35fa212bd740031261284a94
ArishN <arishm@atozerv.com>
2024-01-12 22:34:26 +0530

    api test two

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', 'TestController@testFunc');
+
+Route::get('testtwo', function () {
+    return "hola";
+});
